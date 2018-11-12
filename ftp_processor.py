9328712187d9955f265a57bd7b09fa8fcbcaa096
Dylan Floyd <dylanfloyd@Dylans-MacBook-Pro.local>
2018-11-12 17:24:57 -0500

    added option to save CSV. changed default in __main__ to False for saveCSV param

--- ftp_processor.py
+++ ftp_processor.py
@@ -1,4 +1,3 @@
-import ftplib
 from ftplib import FTP
 import pandas as pd
 import numpy as np
@@ -16,116 +15,91 @@ col_names = ['YEAR',
              'WIND-DIRECTION',
              'WIND-SPEED',
              'SKY-CONDITION',
-             'RAIN-1HR', #liquid precipitation depth (measured in millimeters
+             'RAIN-1HR', #liquid precipitation depth (measured in millimeters) over 1 hour
              'RAIN-6HR']
 
-
-
-def changeFtpDirectory(anFTP, year, newpath='/pub/data/noaa/isd-lite/'):
-    newpath += str(year)
-    anFTP.cwd(newpath)
-    return None
-
-
-
-def gz_to_df(year, gz_filename, ftp_url_dir='ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/'):
-    afilepath = ftp_url_dir + str(year) + '/' + gz_filename
-    print afilepath
-    df = pd.read_csv(afilepath, compression='gzip', header=0, error_bad_lines=False, warn_bad_lines=False,
-                     delim_whitespace=True)
-    # df.columns = range(1, df.shape[1]+1)
-    df.columns = col_names
-    satinfo = gz_filename[0:len(gz_filename)-3]
-    # local_savepath = './data/' + str(year) + '/' + satinfo + '.csv'
-    save_csv(df, year, satinfo)
-    return df
-
-
-def get_multiple_dfs(year, gzfilenames=list(), ftp_url_dir='ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/'):
-    dataframes = []
-    counter = 1
-    numfiles = len(gzfilenames)
-    for gzfile in gzfilenames:
-        try:
-            satellite_info = gzfile[0:len(gzfile)-3]
-            retrieving = 'retrieving ' + str(counter) + ' of ' + str(numfiles) + '...'
-            print(retrieving)
-            df = gz_to_df(year, gzfile)
-            df = dynamic_NaN_removal(df)
-            df.columns = col_names
-            df['SATELLITE-INFO'] = satellite_info
-            # print("dataframe preview:")
-            # print(df.head())
-            # print("shape: ", df.shape)
-            # save_csv(df)
-            dataframes.append(df)
-            counter += 1
-        except:
-            err_msg = "error occured at: " + str(satellite_info)
-            print err_msg
-    return dataframes
-
-#There's supposed to only be 12 relevant columns, but the delimitations in the text files are gross
-#This method captures the 12 columns with the fewest NA values
-def dynamic_NaN_removal(df):
-    nasum = df.isna().sum()
-    nasum_np = np.array(nasum)
-    nasum_np.sort()
-    threshold = int(nasum_np[12])
-    nasum_t = nasum[nasum < threshold]
-    relevant_columns = list(nasum_t.index)
-    newdf = df.iloc[:, relevant_columns]
-    return newdf
-
-
-def save_csv(df, year, satinfo=""):
-    directory = './data/'
-    year_directory = directory + str(year)
-    if not os.path.exists(year_directory):
-        os.makedirs(year_directory)
-    if satinfo == "":
-        satinfo = df.iloc[0, -1]
-    local_savepath = './data/' + str(year) + '/' + satinfo + '.csv'
-    # filepath = './data/' + str(satinfo) + '.csv' #add a folder for year?
-    df.to_csv(local_savepath)
-
-
-
-
+# login
+# This method is used to instantiate an FTP object and navigate the .gov site to get to the noaa/isd-lite folder
 def login():
     my_ftp = FTP('ftp.ncdc.noaa.gov')
     my_ftp.login()
     my_ftp.cwd('/pub/data/noaa/isd-lite/')
     return my_ftp
 
+# changeFtpDirectory
+# This method is used to navigate to a certain folder within the noaa/isd-lite ftp. Able to override inputs to handle
+# a variety of directories and folders. In this case 'year' would be the new folder to navigate to.
+def changeFtpDirectory(anFTP, year, newpath='/pub/data/noaa/isd-lite/'):
+    newpath += str(year)
+    anFTP.cwd(newpath)
+    return None
+
+# gz_to_df
+# Used to import a .gz file from the FTP link and return a dataframe
+# The pandas Dataframe handles uses whitespace as a delimeter to parse the text file
+# saveCSV defaults to true and will save the df to a csv file locally under the year passed to the function
+def gz_to_df(year, gz_filename, ftp_url_dir='ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/', saveCSV=True):
+    afilepath = ftp_url_dir + str(year) + '/' + gz_filename
+    print afilepath
+    df = pd.read_csv(afilepath, compression='gzip', header=0, error_bad_lines=False, delim_whitespace=True)
+    df.columns = col_names
+    satinfo = gz_filename[0:len(gz_filename)-3]
+    if saveCSV:
+        save_csv(df, year, satinfo)
+    return df
 
 
+# dynamic_NaN_removal
+# This method captures n columns with the fewest NA values
+def dynamic_NaN_removal(df, n, verbose=False):
+    nasum = df.isna().sum()
+    nasum_np = np.array(nasum)
+    if verbose:
+        print nasum_np
+    nasum_np.sort()
+    na_threshold = int(nasum_np[n])
+    valid_nasums = nasum[nasum < na_threshold]
+    relevant_columns = list(valid_nasums.index)
+    newdf = df.iloc[:, relevant_columns]
+    return newdf
 
 
+# save_csv
+# This method saves a pandas dataframe to a local folder with a particular filename as a .csv file.
+# If the directory does not already exist, it will create it. Most of the folders created were for a particular year
+def save_csv(df, folder, filename="testsave"):
+    directory = './data/'
+    folder_directory = directory + str(folder)
+    if not os.path.exists(folder_directory):
+        os.makedirs(folder_directory)
+    local_savepath = folder_directory + '/' + filename + '.csv'
+    df.to_csv(local_savepath)
 
 
-
-
-#next step is to get a handful of csvs from each year for some set of satellites
-#have them save original dato the folders
-#do something cool with these DFsj
-def collect_annual_data(anFtp, start_yr, end_yr):
+# collect_annual_data
+# This method takes in the ftp object, and a start and stop year to import NOAA-ISD-Lite data, convert the .txt files
+# to .csv files, and save them locally for further analysis if desired.
+# Empty lists at the top can be used to store information read from the csv files so that the csv's don't have to be
+# stored locally if the end user only wants to gather data in working memory rather than store it on the hard drive.
+# Lists won't be returned until all of the files have been processed.
+def collect_annual_data(anFtp, start_yr, end_yr, saveCSV=True):
     maxtemps = []
     process_times = []
-    dataframes = []
     dates = []
     gzfilenames = []
     for year in range(start_yr, end_yr, 1):
         changeFtpDirectory(anFtp, year)
         dot_gzfiles = anFtp.nlst()
+        num_files = len(dot_gzfiles)
+        i = 0
         for gzfilename in dot_gzfiles:
+            i += 1
             try:
-                runtime_msg = "processing " + str(gzfilename) + '...'
+                runtime_msg = "processing " + str(i) + " of " + str(num_files) + ": " + str(gzfilename) + '...'
                 print runtime_msg
                 before = time.time()
-                adf = gz_to_df(year, gzfilename)
+                adf = gz_to_df(year, gzfilename, saveCSV=saveCSV)
                 after = time.time()
-                # print adf.head()
                 maxtemp = adf['AIR-TEMP'].max()
                 index = adf['AIR-TEMP'].idxmax()
                 maxtemps.append(maxtemp)
@@ -137,63 +111,27 @@ def collect_annual_data(anFtp, start_yr, end_yr):
                 hour= int(adf.iloc[index, :]['HOUR'])
                 dates.append(pd.datetime(year=int(year), month=month, day=day, hour=hour))
             except:
-                pass
-            # dataframes.append(adf)
+                runtime_msg = "Skipped the following file: " + str(gzfilename)
+                print runtime_msg
+
     return [maxtemps, dates, process_times, gzfilenames]
 
 
 #For each satellite, this function gets info on the day with the highest recorded temperature
-def get_hottest_day_in_year(anFTP, year):
-    output = collect_annual_data(anFTP, start_yr=year, end_yr=int(year)+1)
-    maxtemps, dates, process_times, gzfilenames = output
-    # results_df = pd.DataFrame(data=[maxtemps, runtimes, dates], index=filenames)
-    # return results_df
+def get_satellite_maxtemp_by_year(anFTP, year, saveCSV=True):
+    output = collect_annual_data(anFTP, start_yr=year, end_yr=int(year)+1, saveCSV=saveCSV)
+    # maxtemps, dates, process_times, gzfilenames = output
     return output
 
 
-# ftp = FTP('ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite/')
-# ftp = FTP('ftp.ncdc.noaa.gov')
-# ftp.login()
-# ftp.dir()
-# ftp.cwd('/pub/data/noaa/isd-lite')
-# print "-"*30
-# ftp.dir()
-#
-# ftp.cwd('/pub/data/noaa/isd-lite/2016')
-# print "-"*30
-# ftp.dir()
-
-##################uncomment below
-# print "Logging into FTP"
-# ftp = login()
-# ftp.cwd('/pub/data/noaa/isd-lite/2016')
-# # dot_gzfiles = ftp.nlst()
-# # adf = gz_to_df(2016, dot_gzfiles[0])
-# output = get_hottest_day_in_year(ftp, 2016)
-# maxtemps, dates, process_times, filenames = output
-# df = pd.DataFrame()
-# df['dates'] = dates
-# df['filenames'] = filenames
-# df['process_time'] = process_times
-# df['maxtemp'] = maxtemps
-# save_csv(df, 'max_temp_results', 'annual_max_temp_by_satellite')
-
-##################uncomment above
-
-
-# reduced_df = dynamic_NaN_removal(adf)
-# dfs2017 = get_multiple_dfs(2017, dot_gzfiles)
-# get_multiple_dfs(2017, dot_gzfiles[0::100])
-# dfs = get_multiple_dfs(2016, dot_gzfiles[0:2])
 
 if __name__ == '__main__':
     print "Logging into FTP"
     ftp = login()
     ftp.cwd('/pub/data/noaa/isd-lite/2017')
-    # dot_gzfiles = ftp.nlst()
-    # adf = gz_to_df(2016, dot_gzfiles[0])
-    output = get_hottest_day_in_year(ftp, 2017)
+    output = get_satellite_maxtemp_by_year(ftp, 2017, saveCSV=False) #change to True to reproduce csv files.
     maxtemps, dates, process_times, filenames = output
+
     df = pd.DataFrame()
     df['dates'] = dates
     df['filenames'] = filenames
